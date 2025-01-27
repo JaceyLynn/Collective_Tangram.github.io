016b2f7f370832aeaf1c4a12b10abf234c3e7d6c
Glitch (glitch-hello-website) <none>
2025-01-27 18:08:25 +0000

    👄🚒 Checkpoint ./index.js:639386/3932

--- index.js
+++ index.js
@@ -1,93 +1,53 @@
+// Because this is a module script, we can import code from other modules
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
 import * as THREE from "three";
 
-let scene = new THREE.Scene();
+let width = 400;
+let height = 400;
 
-let aspect = window.innerWidth / window.innerHeight;
-let camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+// create a scene container (into which we will add all of the other objects)
+let scene = new THREE.Scene();
+// set a background color for our scene to something other than the default
+scene.background = new THREE.Color(1,0,0.2);
+
+
+
+
+// create a renderer which will draw the scene onto our HTML <canvas> element
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement); // add the renderer to the webpage
+
+
+
+
+// create a camera which can be moved abouyt the scene
+let fieldOfView = 60; // how wide is our camera view in degrees 
+let aspectRatio = width / height;
+let camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, 0.1, 1000);
 camera.position.z = 8; // place the camera in space
 
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
 
-scene.background = new THREE.Color(1,0,0.2);
-
+// create an object with a geometry and material
 const geometry = new THREE.TorusKnotGeometry( 2, 0.5, 100, 16 );
-
-let randomColor = new THREE.Color(Math.random(),Math.random(),Math.random());
-
-const material = new THREE.MeshPhongMaterial({color: randomColor}); 
+let myColor = new THREE.Color("rgb(0,255,255)"); // we will define our color with three numbers (red, green blue) 
+const material = new THREE.MeshBasicMaterial({color: myColor}); 
 const sphere = new THREE.Mesh( geometry, material ); 
-scene.add( sphere );
+scene.add( sphere ); // add it to the scene
 
 
+
+// try adding some lights
 let myLight = new THREE.AmbientLight('0xffffff');
 scene.add(myLight);
 
 // White directional light at half intensity shining from the top.
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.95 );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 scene.add( directionalLight );
 
 
+
+
+// finally, draw our scene to the <canvas> element
 renderer.render(scene, camera);
-
-
-
-
-
-
-
-
-
-
-// // Because this is a module script, we can import code from other modules
-// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules
-// import * as THREE from "three";
-
-// // create a scene container in which all other objects will exist
-// let scene = new THREE.Scene();
-
-// // create a camera and position it in space
-// let aspect = window.innerWidth / window.innerHeight;
-// let camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
-// camera.position.z = 8; // place the camera in space
-
-// // the renderer will actually show the camera view within our <canvas>
-// let renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-
-// // create a sphere
-// let geometry = new THREE.SphereGeometry(1, 12, 12);
-// let material = new THREE.MeshBasicMaterial({ color: "blue" });
-// let myMesh = new THREE.Mesh(geometry, material);
-
-// // and add it to the scene container
-// scene.add(myMesh);
-
-// // what else can we do?
-// // try placing objects with a for-loop
-// // let geo = new THREE.SphereGeometry(0.1, 12, 12);
-// // let mat = new THREE.MeshBasicMaterial({ color: "blue" });
-// // for (let i = -3; i <= 3; i++) {
-// //   for (let j = -3; j <= 3; j++) {
-// //     let mesh = new THREE.Mesh(geo, mat);
-// //     mesh.position.set(j, i, 0);
-// //     mesh.scale.set(Math.abs(i) + 0.1, 1, 1);
-// //     scene.add(mesh);
-// //     console.log(i * 5);
-// //   }
-// // }
-
-// // try changing the background color (or add an image)
-// // scene.background = new THREE.Color("rgb(255, 10, 100)");
-
-// // set a different camera position
-// // camera.position.set(0,-10,10);
-// // camera.lookAt(0,0,0);
-
-// // 
-
-// // finally, take a picture of the scene and show it in the <canvas>
-// renderer.render(scene, camera);
