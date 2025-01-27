700bdbdaa36008de80d89edf6e569cd76cbb2710
Glitch (glitch-hello-website) <none>
2025-01-27 22:44:25 +0000

    🐮👌 Checkpoint ./index.js:639386/6514 ./index.html:639386/1828

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
-// import syntax
-// how to use glitch.com
-// how to use browser console on glitch.com
+// what is glitch.com
+// moving from p5.js to 'vanilla' javascript (i.e. outside of the p5.js ecosystem)
+
+// import syntax - how to use three.js
+// how to use the browser console on glitch.com
 
 // what is 'new' keyword and how does it allow us to create new instances of a class
+
 // x, y, z positions - what is euclidean space / cartesian coordinates
 
+// looking at a three.js example 
+// wrapping everything in an 'init' function
+
+// what is a geometry, material, mesh
+
+// how to position a mesh in 3D space (and set rotation and scale)
 
 import * as THREE from "three";
 
 // console.log("three.js:",THREE);
 
-
-
-
-
 let scene = new THREE.Scene();
-scene.background = new THREE.Color("rgb(255,200,255)")
+scene.background = new THREE.Color("rgb(255,200,255)");
 
 let renderer = new THREE.WebGLRenderer();
-renderer.setSize(800,800);
+renderer.setSize(500, 500);
 document.body.appendChild(renderer.domElement);
 
+let camera = new THREE.PerspectiveCamera(60, 1, 0.1, 1000);
+camera.position.set(10, 10, -10);
+camera.lookAt(0, 0, 0);
 
-let camera = new THREE.PerspectiveCamera(75,1,0.1,1000);
-camera.position.set(0,0,-10)
-camera.lookAt(0,0,0);
+// for (let i = 0; i < 10; i++) {
+//   for (let j = 0; j < 10; j++) {
+//     let geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+//     let mat = new THREE.MeshNormalMaterial();
+//     let mesh = new THREE.Mesh(geometry, mat);
+//     scene.add(mesh);
+//     mesh.position.set(i - 4.5, j - 4.5);
+//   }
+// }
+
+let g = new THREE.BoxGeometry(1,1,1);
+let m = new THREE.MeshNormalMaterial();
+let mesh = new THREE.Mesh(g,m);
+scene.add(mesh);
+mesh.position.set(5,1,5);
+
+let mesh2 = new THREE.Mesh(g,m);
+mesh2.position.set(2,3,4);
+scene.add(mesh2);
+
+let mesh3 = new THREE.Mesh(g,m);
+mesh3.position.set(4,3,6);
+scene.add(mesh3);
+
+
+renderer.render(scene, camera);
 
 
 
-for (let i = 0; i < 10; i++){
-  for (let j = 0; j < 10; j++){
-  let geometry = new THREE.BoxGeometry(0.5,0.5,0.5);
-  let mat = new THREE.MeshNormalMaterial();
-  let mesh = new THREE.Mesh(geometry,mat);
-  scene.add(mesh);
-  mesh.position.set(i-5,j-5,)
-    }
-}
 
-renderer.render(scene,camera);
+
+
+
+
 
 
 
@@ -96,33 +121,23 @@ renderer.render(scene,camera);
 // // set a background color for our scene to something other than the default
 // scene.background = new THREE.Color(1,0,0.2);
 
-
-
-
 // // create a renderer which will draw the scene onto our HTML <canvas> element
 // let renderer = new THREE.WebGLRenderer();
 // renderer.setSize(width, height);
 // document.body.appendChild(renderer.domElement); // add the renderer to the webpage
 
-
-
-
 // // create a camera which can be moved abouyt the scene
-// let fieldOfView = 60; // how wide is our camera view in degrees 
+// let fieldOfView = 60; // how wide is our camera view in degrees
 // let aspectRatio = width / height;
 // let camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, 0.1, 1000);
 // camera.position.z = 8; // place the camera in space
 
-
-
 // // create an object with a geometry and material
 // const geometry = new THREE.TorusKnotGeometry( 2, 0.5, 100, 16 );
-// let myColor = new THREE.Color("rgb(0,255,255)"); // we will define our color with three numbers (red, green blue) 
-// const material = new THREE.MeshBasicMaterial({color: myColor}); 
-// const sphere = new THREE.Mesh( geometry, material ); 
+// let myColor = new THREE.Color("rgb(0,255,255)"); // we will define our color with three numbers (red, green blue)
+// const material = new THREE.MeshBasicMaterial({color: myColor});
+// const sphere = new THREE.Mesh( geometry, material );
 // scene.add( sphere ); // add it to the scene
-
-
 
 // // try adding some lights
 // let myLight = new THREE.AmbientLight('0xffffff');
@@ -131,9 +146,6 @@ renderer.render(scene,camera);
 // // White directional light at half intensity shining from the top.
 // const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 // scene.add( directionalLight );
-
-
-
 
 // // finally, draw our scene to the <canvas> element
 // renderer.render(scene, camera);
