646531f1975122f6433348a7313f09405b56ea77
Glitch (glitch-hello-website) <none>
2025-02-16 01:10:47 +0000

    💧😹 Checkpoint ./index.js:74023647/5165

--- index.js
+++ index.js
@@ -40,59 +40,12 @@ directionalLight.shadow.mapSize.height = 512; // default
 directionalLight.shadow.camera.near = 0.5; // default
 directionalLight.shadow.camera.far = 500; // default
 
-  // Add a cone to the scene
-  const coneGeometry = new THREE.ConeGeometry(2, 8, 32);
-  const coneMaterial = new THREE.MeshStandardMaterial({
-    color: "#FF8600",
-    roughness: 0.7,
-  });
-  const cone = new THREE.Mesh(coneGeometry, coneMaterial);
-  cone.position.set(-2, 2, 0);
-  cone.castShadow = true;
-  coneGeometry.rotateX(-2);
-  coneGeometry.rotateZ(7);
-  scene.add(cone);
-
-  // Add a large sphere
-  const sphereGeometry = new THREE.SphereGeometry(9, 32, 32);
-  const sphereMaterial = new THREE.MeshStandardMaterial({
-    color: "#AEB8FE",
-    roughness: 1,
-  });
-  const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-  sphere.position.set(5, -2, -10);
-  sphere.castShadow = true;
-  scene.add(sphere);
-
-  // Add a long cylinder
-  const cylinderGeometry = new THREE.CylinderGeometry(0.5, 0.5, 38, 32);
-  const cylinderMaterial = new THREE.MeshStandardMaterial({
-    color: "#27187E",
-    roughness: 1,
-  });
-  const cylinder = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-  cylinder.position.set(4, -1, -20);
-  cylinder.rotation.z = Math.PI / 4;
-  cylinder.castShadow = true;
-  cylinderGeometry.rotateX(0);
-  cylinderGeometry.rotateZ(-30);
-  scene.add(cylinder);
-
-  // Add a small transparent glass cylinder
-  const cylinderGeometry2 = new THREE.CylinderGeometry(1, 1, 0.2, 32);
-  const cylinderMaterial2 = new THREE.MeshStandardMaterial({
-    color: "#FFFFFF",
-    transparent: true,
-    opacity: 0.4,
-    roughness: 0.1,
-  });
-  const cylinder2 = new THREE.Mesh(cylinderGeometry2, cylinderMaterial2);
-  cylinder2.position.set(0, -3.5, 15);
-  cylinder2.rotation.z = Math.PI / 4;
-  cylinder2.castShadow = true;
-  cylinderGeometry2.rotateX(1);
-  cylinderGeometry2.rotateZ(-4);
-  scene.add(cylinder2);
+const circlegeometry = new THREE.CircleGeometry( 5, 32 ); 
+const circlematerial = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+const floor = new THREE.Mesh( circlegeometry, circlematerial ); 
+  floor.rotateX(-3.14/2);
+  floor.position.set(0,0, 0);
+  scene.add( floor );
 
   // Add a short solid cylinder
   const cylinderGeometry3 = new THREE.CylinderGeometry(0.5, 0.5, 4, 32);
@@ -129,91 +82,10 @@ directionalLight.shadow.camera.far = 500; // default
   });
   const ceiling = new THREE.Mesh(ringgeometry, ringMaterial);
   ceiling.rotateX(-3.14/2);
-  ceiling.position.set(1, -2, 8);
+  ceiling.position.set(0,3, 0);
   scene.add(ceiling);
 
-  // Add a polyhedron
-  const vertices = [
-    1,
-    1,
-    1,
-    -1,
-    -1,
-    1,
-    -1,
-    1,
-    -1,
-    1,
-    -1,
-    -1, // Vertex positions
-  ];
-  const indices = [
-    2,
-    1,
-    0,
-    0,
-    3,
-    2,
-    1,
-    3,
-    0,
-    2,
-    3,
-    1, // Face definitions
-  ];
-  const polyhedronGeometry = new THREE.PolyhedronGeometry(
-    vertices,
-    indices,
-    1.5,
-    2
-  );
-  const polyhedronMaterial = new THREE.MeshStandardMaterial({
-    color: "#FFFFFF",
-    transparent: true,
-    opacity: 0.4,
-    roughness: 0.1,
-  });
-  const polyhedron = new THREE.Mesh(polyhedronGeometry, polyhedronMaterial);
-  polyhedron.position.set(7, 8, -2);
-  polyhedron.castShadow = true;
-  scene.add(polyhedron);
-  
-
-const loader = new FontLoader();
-const font = loader.load(
-	// resource URL
-	'fonts/helvetiker_bold.typeface.json',
-
-	// onLoad callback
-	function ( font ) {
-		// do something with the font
-		console.log( font );
-	},
-
-	// onProgress callback
-	function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},
-
-	// onError callback
-	function ( err ) {
-		console.log( 'An error happened' );
-	}
-);
-loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
-
-	const geometry = new TextGeometry( 'Hello three.js!', {
-		font: font,
-		size: 1000,
-		depth: 5,
-		curveSegments: 12,
-		bevelEnabled: true,
-		bevelThickness: 10,
-		bevelSize: 8,
-		bevelOffset: 0,
-		bevelSegments: 5
-	} );
-} );
+ 
   
   draw();
 }
