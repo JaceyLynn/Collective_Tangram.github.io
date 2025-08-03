001a6ce6526dd09ef491f3bc84515850faa85613
Glitch (glitch-hello-website) <none>
2025-08-03 15:06:31 -0400

    Prepare for push to GitHub main branch

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const mongoose = require("mongoose");
+import { connectDB } from "./db.js";
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 app.use(express.static('public')); 
+
+// Connect to MongoDB before starting the server
+connectDB();
 
 // 2) Mongoose schemas & models
 const { Schema, model } = mongoose;
